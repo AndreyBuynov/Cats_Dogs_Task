ad2883fb27d03dde55370e84aadce7689bcdde47
AndreyBuynov <andreybuynovskiy@yahoo.com>
2021-04-28 01:20:05 +0300

    edited

--- utils.py
+++ utils.py
@@ -66,7 +66,7 @@ def plot_img(img_path, isboxes, model = None, device = torch.device('cuda')):
 def plot_img_fastrcnn(img_path, model, isboxes = None, device = torch.device('cuda')):
     labels_dict = {
         1 : 'cat',
-        0 : 'dog'
+        2 : 'dog'
     }
 
     numpy_img = cv2.imread(img_path)[:,:,::-1]
@@ -86,32 +86,26 @@ def plot_img_fastrcnn(img_path, model, isboxes = None, device = torch.device('cu
         predictions = model(img[None,...].to(device))
         preds = predictions[0]
       
-        CONF_THRESH = 0.7
-        boxes = preds['boxes'][preds['scores'] > CONF_THRESH]
+        boxes = preds['boxes'][:1]
         boxes_dict = {}
         boxes_dict['boxes'] = boxes
 
         labels = preds['labels'].cpu().detach().numpy()
-        scores = preds['scores'].cpu().detach().numpy()
 
     for i, box in enumerate(boxes):
         if labels[i] in labels_dict:
-            numpy_img = cv2.putText(numpy_img, labels_dict.get(labels[i]), (int(box[0])+5,int(box[1])-5), font, 1, (255,255,0), 2)
+            numpy_img = cv2.putText(numpy_img, labels_dict.get(labels[i]), (int(box[0])+5,int(box[1])-5), font, 1, (255,255,0), 1)
         else:
-            numpy_img = cv2.putText(numpy_img, str(labels[i]), (int(box[0])+5,int(box[1])-5), font, 1, (255,255,0), 2)
-        #numpy_img = cv2.putText(numpy_img, str(round(scores[i], 2)), (int(box[0])+5,int(box[1])-45), font, 1, (255,255,0), 2)  
+            numpy_img = cv2.putText(numpy_img, str(labels[i]), (int(box[0])+5,int(box[1])-5), font, 1, (255,255,0), 1)
         numpy_img = cv2.rectangle(
             numpy_img, 
             (box[0],box[1]),
             (box[2],box[3]), 
             255,
-            i+2
+            i
         )
 
-    if len(boxes) > 0:
-        return numpy_img #.get()
-    else:
-        return numpy_img
+    return numpy_img
     
     
 def show_random_pict(df, model = None, val_idx = None):
